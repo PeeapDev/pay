14a817da10038c9340e4f93ec62868b19f8395fd
PeeapDev <dev@peeap.com>
2024-09-15 14:45:00 +0000

    first commit

---
+++ Modules/Woocommerce/Config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'name' => 'Woocommerce',
+    'item_id' => 'hxrku28ngdp',
+    'options' => [
+        [
+            'label' => 'Configure', 
+            'url' => url(config('paymoney.prefix') . '/addon/woocommerce/configure')
+        ]
+    ],
+    'supported_versions' => "4.0.0"
+];
